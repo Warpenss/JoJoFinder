a347f532064387e92f685ac595ed754832db1348
Eugene Smyrnov <helldaf@yandex.ru>
2017-06-22 23:48:34 +0300

    Finally I figured out the HtmlUnit, get titles and url's

--- src/main/java/Main/Controllers/FinderController.java
+++ src/main/java/Main/Controllers/FinderController.java
@@ -3,10 +3,8 @@ package Main.Controllers;
 import Main.Entities.JobSite;
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlUnorderedList;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,21 +35,23 @@ public class FinderController {
             //
         }
 
-        String result = "Result: \n";
+        List<JobSite> result = new ArrayList<>();
 
-        List<HtmlElement> vacancies = page.getByXPath("//li[@class='search-result-item']");
-        for (int i = 0; i < vacancies.size(); i++) {
-            HtmlAnchor anchor = vacancies.get(i).getFirstByXPath("/div/a");
-            String url = anchor.getAttribute("href");
+        List<HtmlElement> vacancies = page.getByXPath("//li[@class='search-result-item']" +
+                "/div[contains(@class, 'position-name')]/a");
+        for(HtmlElement vacancy : vacancies) {
             try {
-                URL fullURL = page.getFullyQualifiedUrl(url);
-                result += fullURL;
+                URL fullURL = page.getFullyQualifiedUrl(vacancy.getAttribute("href"));
+                String title = vacancy.getTextContent();
+                result.add(new JobSite(title, fullURL.toString()));
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
+
         }
 
-        return result;
+
+        return result.toString();
     }
 
 }
