88a6b24820203bdc147a5fbf23817d554d41c133
Eugene Smyrnov <helldaf@yandex.ru>
2017-06-26 00:15:52 +0300

    Softserve added

--- src/main/java/Main/Controllers/FinderController.java
+++ src/main/java/Main/Controllers/FinderController.java
@@ -34,16 +34,24 @@ public class FinderController {
                 "arrFilter_pf%5Bcities%5D%5B%5D=11&" +
                 "arrFilter_pf%5Bcategories%5D=95&" +
                 "set_filter=Y#filter-form";
+        String urlSoftserve = "https://softserve.ua/ru/vacancies/open-vacancies/?" +
+                "tax-direction=21&" +
+                "tax-lang=175&" +
+                "tax-country=117&" +
+                "tax-city=121";
 
         String xPathEpam = "//li[@class='search-result-item']/div[contains(@class, 'position-name')]/a";
         String xPathLuxoft = "//a[@class='js-tracking']";
+        String xPathSoftserve = "//a[@class='card-vacancy-link']";
 
         HtmlPage pageEpam = null;
         HtmlPage pageLuxoft = null;
+        HtmlPage pageSoftServe = null;
 
         try (final WebClient webClient = new WebClient(BrowserVersion.CHROME)) {
             pageEpam = webClient.getPage(urlEpam);
             pageLuxoft = webClient.getPage(urlLuxoft);
+            pageSoftServe = webClient.getPage(urlSoftserve);
             Thread.sleep(3_000);
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
@@ -51,22 +59,33 @@ public class FinderController {
 
         if (pageEpam != null) {
             List<HtmlElement> vacanciesEpam = pageEpam.getByXPath(xPathEpam);
-            addVacancies(result, vacanciesEpam, pageEpam);
+            addVacancies("Epam", result, vacanciesEpam, pageEpam);
         }
 
         if (pageLuxoft != null) {
             List<HtmlElement> vacanciesLuxoft = pageLuxoft.getByXPath(xPathLuxoft);
-            addVacancies(result, vacanciesLuxoft, pageLuxoft);
+            addVacancies("Luxoft", result, vacanciesLuxoft, pageLuxoft);
+        }
+
+        if (pageSoftServe != null) {
+            List<HtmlElement> vacanciesSoftserve = pageSoftServe.getByXPath(xPathSoftserve);
+            addVacancies("Softserve", result, vacanciesSoftserve, pageLuxoft);
         }
 
         return result.toString();
     }
 
-    private void addVacancies(List<JobSite> result, List<HtmlElement> vacancies, HtmlPage page) {
+    private void addVacancies(String company, List<JobSite> result, List<HtmlElement> vacancies, HtmlPage page) {
         for (HtmlElement vacancy : vacancies) {
             try {
                 URL fullURL = page.getFullyQualifiedUrl(vacancy.getAttribute("href"));
-                String title = vacancy.getTextContent();
+                String title = "";
+                if (!company.equalsIgnoreCase("Softserve")) {
+                    title = vacancy.getTextContent();
+                }
+                else {
+                    title = vacancy.getFirstElementChild().getFirstElementChild().getTextContent();
+                }
                 result.add(new JobSite(title, fullURL.toString()));
             } catch (MalformedURLException e) {
                 e.printStackTrace();
