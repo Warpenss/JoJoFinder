5f8a6da455542ee75bc729371aa99db936021470
Eugene Smyrnov <helldaf@yandex.ru>
2017-12-16 21:42:08 +0200

    Filtering by City, Company and Language is added.

--- src/main/java/Main/Controllers/FinderController.java
+++ src/main/java/Main/Controllers/FinderController.java
@@ -11,9 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class FinderController {
@@ -38,8 +36,78 @@ public class FinderController {
 
         List<JobSite> jobsList = jobRepository.findByTitleIgnoreCaseContaining(params.getFirst("title"));
 
+        HashSet<JobSite> resultCompany = new HashSet<>();
+        HashSet<JobSite> resultCity = new HashSet<>();
+        HashSet<JobSite> resultLanguage = new HashSet<>();
+
+        boolean resultFlag = false;
 
 
+
+        System.out.println(params);
+
+        for (Map.Entry<String, List<String>> entry : params.entrySet())
+        {
+            if (entry.getKey().equals("company")) {
+                for (String parameter : entry.getValue()) {
+                    for (JobSite job : jobsList) {
+                        if (job.getCompany().equals(parameter)) {
+                            resultCompany.add(job);
+                        }
+                    }
+                }
+                if (resultCompany.isEmpty()) {
+                    resultFlag = true;
+                }
+            }
+
+            if (entry.getKey().equals("city")) {
+                for (String parameter : entry.getValue()) {
+                    if (resultCompany.isEmpty()) {
+                        resultCompany.addAll(jobsList);
+                    }
+                    for (JobSite job : resultCompany) {
+                        if (job.getCity().equals(parameter)) {
+                            resultCity.add(job);
+                        }
+                    }
+                }
+                if (resultCity.isEmpty()) {
+                    resultFlag = true;
+                }
+            }
+
+
+            if (entry.getKey().equals("language")) {
+                for (String parameter : entry.getValue()) {
+                    if (resultCity.isEmpty()) {
+                        resultCity.addAll(jobsList);
+                    }
+                    for (JobSite job : resultCity) {
+                        if (job.getLanguage().equals(parameter)) {
+                            resultLanguage.add(job);
+                        }
+                    }
+
+                }
+                if (resultLanguage.isEmpty()) {
+                    resultFlag = true;
+                }
+            }
+        }
+
+        if (resultLanguage.isEmpty() && !resultFlag) {
+            resultLanguage.addAll(resultCity);
+        }
+        if (resultLanguage.isEmpty() && !resultFlag) {
+            resultLanguage.addAll(resultCompany);
+        }
+        if (resultLanguage.isEmpty() && !resultFlag) {
+            resultLanguage.addAll(jobsList);
+        }
+
+        jobsList.clear();
+        jobsList.addAll(resultLanguage);
 
         jobsList.sort(Comparator.comparing(JobSite::getTime).reversed());
         model.addAttribute("jobs", jobsList);
